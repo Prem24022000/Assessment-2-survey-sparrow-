e48672d9337b06de6e1563bc4563e20ad70d82b5
Prem Prakash Sahu <69319430+Prem24022000@users.noreply.github.com>
2023-01-16 17:20:03 +0530

    Add files via upload

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,26 +1,7 @@
-const expands = document.querySelectorAll('.expand');
-const enhancementParagraphs = document.querySelector('.enhancement__page__mid--paragraph');
+const contentBxs = document.querySelectorAll('.enhancement__page__mid--contentBx');
 
-let value = false;
-
-expands.forEach((expand) => {
-    expand.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        const paragraph = e.target.parentElement.nextElementSibling;
-    
-        if(!value){
-            e.target.innerText = '-';
-            value = true;
-            paragraph.classList.add('paragraph');
-        }else{
-            e.target.innerText = '+';
-            value = false;
-            paragraph.classList.remove('paragraph');
-
-        }
+contentBxs.forEach((contnetBx) => {
+    contnetBx.addEventListener('click', function(){
+        this.classList.toggle('active');
     })
 })
-
-
-
