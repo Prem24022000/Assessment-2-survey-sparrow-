c58bff581a9d3cd42e4e6080b4c60325c5f92bbf
Prem Prakash Sahu <69319430+Prem24022000@users.noreply.github.com>
2023-01-18 14:48:17 +0530

    Add files via upload
    Responsiveness

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,17 @@
 const contentBxs = document.querySelectorAll('.enhancement__page__mid--contentBx');
 
+const burgerIcon = document.querySelector('.burger');
+
+const navs = document.querySelector('.nav__items--navs');
+
+console.log(navs);
+
 contentBxs.forEach((contnetBx) => {
     contnetBx.addEventListener('click', function(){
         this.classList.toggle('active');
     })
+})
+
+burgerIcon.addEventListener('click', () => {
+    navs.classList.toggle('active');
 })
